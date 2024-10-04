56bd7c050b7dfd0545ffcdae78c101548d98f701
KauanMO <kauaanmatheus@gmail.com>
2024-10-03 21:28:23 -0300

    feat: Moving TokenService to /impl

--- GameReview/Services/Impl/TokenService.cs
+++ GameReview/Services/Impl/TokenService.cs
@@ -4,7 +4,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace GameReview.Services;
+namespace GameReview.Services.Impl;
 
 public class TokenService(IConfiguration configuration)
 {
