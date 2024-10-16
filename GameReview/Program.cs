cbbeba4dd409255b7500f51b32b6f66f1da811ae
KauanMO <kauaanmatheus@gmail.com>
2024-10-16 20:12:07 -0300

    feat: CORS Polity

--- GameReview/Program.cs
+++ GameReview/Program.cs
@@ -13,6 +13,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(opts =>
+{
+    opts.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5173");
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowCredentials();
+    });
+});
+
 builder.Services.AddControllers(opts =>
 {
     opts.Filters.Add(new GlobalExceptionFilter());
@@ -46,10 +57,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
+app.UseCors();
 
 app.Run();
